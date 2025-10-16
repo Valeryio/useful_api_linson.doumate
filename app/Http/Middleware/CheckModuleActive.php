afc0078c998958dd5eb0be784b7a5d6d8052324e
valeryio <yonnival0.8@gmail.com>
2025-10-16 16:15:43 +0100

    feat (checkModuleActiveMiddleware):
    Check if the user's module is activated or not

---
+++ app/Http/Middleware/CheckModuleActive.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\UserModules;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckModuleActive
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $moduleId = $request["id"];
+        $user = Auth::user();
+
+        $existingUserModule = UserModules::where("user_id", $user->id)
+            ->where("module_id", $moduleId)
+            ->first();
+
+        if (!$existingUserModule) {
+            return response([
+                "error" => "Not found!"
+            ], 404);
+        }
+
+        if ($existingUserModule["active"] == 0 )
+        {
+            return response([
+                "error" => "Module inactive. Please activate this module to use it."
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\Modules\ModuleController;
+use App\Http\Middleware\CheckModuleActive;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
