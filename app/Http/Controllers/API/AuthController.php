cc27eee5d4c43aeaec044cb31d891bc3463856ab
valeryio <yonnival0.8@gmail.com>
2025-10-16 12:29:45 +0100

    feat (auth):
    Basic auth system have been added to the project

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,29 +5,60 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\Auth\UserLoginRequest;
+use App\Http\Requests\Auth\UserRegistrationRequest;
+use App\Models\User;
 use Dotenv\Exception\ValidationException;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+
+    public function register(UserRegistrationRequest $request) {
+
+        $fields = $request->all();
+        $user = User::create($fields);
+        
+        $data = [
+            "id" => $user->id,
+            "name" => $user->name,
+            "email" => $user->email,
+            "created_at" => $user->created_at
+        ];
+
+        return [
+            "user" => $data
+        ];
+    }
+
+
     public function login(UserLoginRequest $request) {
+        $existingUser = User::where("email", $request["email"])->first();
 
-        $credentials = $request->all();
-
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            return response()->json([
-                "success" => true,
-            ]);
+        if (!$existingUser || !Hash::check($request->password, $existingUser->password)){
+            return [
+                "message" => "Provided values are incorrect!"
+            ];
         }
 
-        throw ValidationException::withMessage([
-            "message" => "Wrong credentials!"
-        ]
-        );
+        $data = [
+            "id" => $existingUser->id,
+            "email" => $existingUser->email,
+            "created_at" => $existingUser->created_at
+        ];
+        $token = $existingUser->createToken($existingUser->id);
+         return [
+            "user" => $data,
+            "token" => $token->plainTextToken
+        ];
     }
 
-    public function register() {
+    public function logout(Request $request) {
+        $request->user()->tokens()->delete();
 
+        return [
+            "message" => "User logout successfully!"
+        ];
     }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post("/register", [AuthController::class, "register"])
+    ->name("register");
 
-Route::prefix("auth")
+Route::post("login", [AuthController::class, "login"])
+    ->name("login");
 
-    ->group(function () {
-        Route::post("login", [AuthController::class, "login"]);
-        
-    });
+
+
+Route::post("logout", [AuthController::class, "logout"])
+    ->name("logout")
+    ->middleware('auth:sanctum');
